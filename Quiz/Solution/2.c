e72dff9633ea254b080091a10191558f736c766d
Sara Younesi <74740665+Sarayounesi@users.noreply.github.com>
2024-08-24 23:28:10 +0330

    Add files via upload

---
+++ Quiz/Solution/2.c
@@ -0,0 +1,17 @@
+
+#include <stdio.h>
+
+void swap(int *a, int *b)
+{
+    int tmp = *a;
+    *a = *b;
+    *b = tmp;
+}
+int main()
+{
+    int a = 11;
+    int b = 1;
+    printf("a before swap:%d b before swap:%d\n", a, b);
+    swap(&a, &b);
+    printf("a after swap:%d b after swap:%d\n", a, b);
+}
